2996d1c7c87b2375983a4cd8c08ad07229164202
Darek Pojda <dpojda@iitis.pl>
2026-06-01 10:07:46 +0200

    ..

---
+++ plugins/virtRTG/pluginMain.py
@@ -0,0 +1,80 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 22 11:08:03 2023
+
+@author: pojdulos
+"""
+
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
+from PyQt5.QtWidgets import *
+
+from dpVision import AP, PluginInterface
+
+class Plugin01(PluginInterface):
+	def __init__(self):
+		self.plugin_name = '(dp) Virtual RTG'
+		self.panel = None
+
+	def on_load(self):
+		print( "plugin "+self.plugin_name+" loaded.")
+		
+		# self.mainWindow.helpAbout()
+		self.add_menu()
+		# self.create_panel(AP.mainWin)
+
+	def create_panel(self, parent):
+		self.panel = QDockWidget(parent)
+		self.panel.setWindowTitle(self.plugin_name)
+		AP.mainWin.addDockWidget(Qt.DockWidgetArea.RightDockWidgetArea, self.panel)
+
+	def on_unload(self):
+		self.remove_menu()
+		if self.panel:
+			AP.mainWin.removeDockWidget(self.panel);    
+		print( "Bye bye")
+		
+	def add_menu(self):
+		nadrzedne_menu = self.add_plugins_menu()
+
+		self.menu = QMenu("Virtual RTG", AP.mainWin)
+		
+		nadrzedne_menu.addMenu(self.menu)
+		
+		action = QAction("UnLoad", AP.mainWin)
+		self.menu.addAction(action)
+		action.triggered.connect(self.onAction_UnLoad)
+		
+	def remove_menu(self):
+		# Znajdź "NadrzędneMenu"
+		nadrzedne_menu = None
+		for action in AP.mainWin.menuBar.actions():
+			if action.text() == "Plugins":
+				nadrzedne_menu = action.menu()
+				break
+		
+		if nadrzedne_menu:
+			# Znajdź i usuń "Plugin01"
+			plugin_menu_action = None
+			for action in nadrzedne_menu.actions():
+				if action.text() == "Virtual RTG" and action.menu():
+					plugin_menu_action = action
+					break
+			
+			if plugin_menu_action:
+				nadrzedne_menu.removeAction(plugin_menu_action)
+		
+		self.remove_plugins_menu()
+		
+
+	def onAction_UnLoad(self):
+		print("Akcja manu: Wyładuj plugin")
+		AP.mainApp.unload_plugin(self)
+		
+		
+	def perform_action(self):
+		print("Akcja wykonana przez "+self.plugin_name)
+
+	def on_button1(self):
+		#QMessageBox.information(self.mainWindow, 'Komunikat', 'Akcja wykonana przez '+self.plugin_name)
+		pass
